f30d8d0de0d1848b098b1e5f6d9d32794dddf948
pvgomes <paulo.gomes@nubank.com.br>
2022-07-19 08:48:19 -0300

    first working flow

--- src/nu_tik_talk_bot/core.clj
+++ src/nu_tik_talk_bot/core.clj
@@ -11,10 +11,13 @@
 
 (def token (env :telegram-token))
 
+(defn chama-lala [url]
+  (println url))
+
 (defn population-by-country-and-year [country-code year]
-  (let [url (str "https://servicodados.ibge.gov.br/api/v1/paises/" country-code "/indicadores/77849")
-        raw-response (:body (http-client/get url
-                                             {:query-params {"periodo" year}}))
+  (let [url (str "https://servicodados.ibge.gov.br/api/v1/paises/" country-code "/indicadores/77849?periodo=" year)
+        lala (chama-lala url)
+        raw-response (:body (http-client/get url))
         response (->> raw-response
                       json/read-json
                       first)
@@ -24,6 +27,14 @@
                              (get-in [:serie])
                              first )]
     ((keyword year) population-years)))
+
+
+(defn handle-population [params]
+  (let [params-array (str/split params #" ")
+        country-code (nth params-array 1)
+        year (nth params-array 2)
+        response (population-by-country-and-year country-code year)]
+    (str "Total population in " year " are " response)))
 
 (h/defhandler handler
 
@@ -39,12 +50,7 @@
 
   (h/command-fn "population"
     (fn [{{id :id :as chat} :chat :as message}]
-      (let [params (str/split message #" ")
-            country-code (nth params 1)
-            year (nth params 2)
-            response (population-by-country-and-year country-code year)
-            response-message (str "Total population in " year "are" response)]
-        (t/send-text token id response-message))))
+      (t/send-text token id (handle-population (:text message)))))
 
   (h/message-fn
     (fn [{{id :id} :chat :as message}]
@@ -67,6 +73,39 @@
   (str/split "/population BR 2020" #" ")
 
   (nth (str/split "/population BR 2020" #" ") 1)
+
+
+  ;message mock
+  ;{:message_id 71, :from {:id 746460687, :is_bot false, :first_name Paulo Victor, :last_name Gomes, :username pvgomes, :language_code en}, :chat {:id 746460687, :first_name Paulo Victor, :last_name Gomes, :username pvgomes, :type private}, :date 1658229947, :text /population BR 2022, :entities [{:offset 0, :length 11, :type bot_command}]}
+
+  (def message-mock
+    {:message_id 71
+     :from {:id 746460687
+            :is_bot false
+            :first_name "Paulo"
+            :last_name "Gomes"
+            :username "pvgomes"
+            :language_code "en"}
+     :chat {:id 746460687
+            :first_name "Paulo"
+            :last_name "Gomes"
+            :username "pvgomes"
+            :type "private"}
+     :date 1658229947
+     :text "/population BR 2020"
+     :entities [{
+                 :offset 0
+                 :length 11
+                 :type "bot_command"}]})
+
+
+  (println (handle-population (:text message-mock)))
+
+  ; https://servicodados.ibge.gov.br/api/v1/paises/BR/indicadores/77849?indicadores=2022
+
+
+
+
 
 
   )
